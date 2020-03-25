ec7004210805a6b529bc3f4c6a806e3f8bd6bc68
padmanabh <wattamwarp874@gmail.com>
2020-03-25 14:27:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+int a=0,b=0,c=102,d=255;
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: "the calculator app",
+      theme: new ThemeData(
+      ),
+
+    );
+  }
+
+}
