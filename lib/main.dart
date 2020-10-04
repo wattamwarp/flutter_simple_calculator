4c6b48c5e689a3df1a8a8026a6710766afbff8bf
Shree <wattamwarp874@gmail.com>
2020-10-04 09:46:39 +0530

    commit msg

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,23 @@ int a=0,b=0,c=102,d=255;
   Widget build(BuildContext context) {
 
     return new MaterialApp(
-      title: "the calculator app",
-      theme: new ThemeData(
+      title: "the calculator app testing pr",
+      theme: new ThemeData( primarySwatch: Colors.red
       ),
 
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('testing',),
+
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text("hi man"),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
